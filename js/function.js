7ffdd98fee8b1620439538befff7fdbe4dc99dce
yoieh <yoieh@live.se>
2016-11-19 15:44:12 +0100

    Git re written the index with the meterilaze grid!

--- js/function.js
+++ js/function.js
@@ -1,20 +1,34 @@
+jQuery.fn.center = function(parent, durationLength) {
+    if (parent) {
+        parent = this.parent();
+    } else {
+        parent = window;
+    }
+    this.animate({
+        top: ((($(parent).height() - this.outerHeight()) / 2) + $(parent).scrollTop() + "px"),
+        left: ((($(parent).width() - this.outerWidth()) / 2) + $(parent).scrollLeft() + "px")
+    }, {duration: durationLength, queue: false});
+return this;
+}
+
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 1000);
+}
 
 $(document).ready(function(){
-  $(document).ready(function(){
-      $('ul.tabs').tabs();
-    });
+  $('ul.tabs').tabs();
 
   $('.carousel').carousel({
       'time_constant': 5000,
       'shift': 1,
       'padding': 200,
       'dist': -100,
-      'no_wrap': false
+      'no_wrap': false,
+      'height' : 300
   });
-  function autoplay() {
-      $('.carousel').carousel('next');
-      setTimeout(autoplay, 1000);
-  }
+
+
   autoplay();
 });
 
@@ -23,35 +37,40 @@ $(window).scroll(function(){
 
   $('.parallax_btn').css({
     'transition': '.5s ease-out',
-    'transform' : 'translate(0px, '+ wScroll / 4 +'%)'
+    'transform' : 'translate(0px, +'+ wScroll / 4 +'%)'
   });
 
   $('.cat-1').css({
     'transition': '.5s cubic-bezier(0, 0, 0.41, 1.11)',
-    'transform' : 'translate(0px, '+ wScroll / 1 +'%)',
+    'transform' : 'translate(0px, +'+ wScroll / 4 +'%)',
   });
 
   $('.parallax_row-1').css({
     'transition': '.5s ease-out',
-    'transform' : 'translate(0px, -'+ wScroll / 4 +'%)',
+    'transform' : 'translate(0px, -'+ wScroll / 8 +'%)',
   });
 
+/*
   $('.parallax_row-2').css({
     'transform' : 'translate(0px, +'+ wScroll / 50 +'%)',
     'transition': '.5s ease-out',
   });
+*/
 
   $('.parallax_row-3').css({
-    'transform' : 'translate(0px, -'+ wScroll / 5 +'%)',
+    'transition': '.5s ease-out',
+    'transform' : 'translate(0px, +'+ wScroll / 2 +'%)',
   });
 
   /*if (!wScroll > $('.first-card').offset().left - ($(window).width() / 2)){
+    $('.first-card').show("scale",{},3000).center(false, 3000);
+
     $('.first-card').css({
       'transition': '.5s ease-out',
       'opacity': wScroll / 400,
       'transform' : 'translate( +'+ wScroll / 5 +'%, 0px)',
     });
-  } else {
+  /*} else {
     $('.first-card').css({
       'left': $(window).width() / 2,
     });
